4020c78a7947bd3ddb234d2ba3615073f27254b9
Akbota <omarbek.akbota@gmail.com>
2023-08-29 11:27:28 +0600

    Initial Commit

---
+++ Home work 13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 13
+//
+//  Created by Akbota Omarbek on 29.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
